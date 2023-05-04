92cae3915f3e7fa01c5b8f2ea8d9cac22f253464
zeeshan.mushtaq <zeeshan.mushtaq4506@gmail.com>
2023-05-04 12:07:23 +0500

    post microservice called from user

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 const connectDB = require('./db.js')
 const { default: mongoose } = require('mongoose');
+const axios = require('axios');
 
 connectDB();
-
+var userID = '';
  // Define User schema
  const userSchema = new mongoose.Schema({
     username: String,
@@ -32,7 +33,9 @@ app.post('/login', async (req, res) => {
       const { username, password } = req.body;
       const user = await User.findOne({ username, password });
       if (user) {
+        userID = user.username
         res.send('User authenticated!');
+
       } else {
         res.status(401).send('Invalid username or password');
       }
@@ -41,6 +44,40 @@ app.post('/login', async (req, res) => {
       res.status(500).send('Server error');
     }
   });
+
+  //Calling Post Microservice by creating a post.
+
+  app.post('/create-post', async (req, res) => {
+    if(userID){
+      const { title, body } = req.body;
+      const postData = {
+        title: title,
+        body: body,
+        userId: userID
+
+      }
+        axios.post('http://localhost:3001/posts', postData)
+        .then(response => {
+          console.log(response.data);
+          // Handle successful response from post microservice
+          res.send('Post created successfully');
+        })
+        .catch(error => {
+          console.error(error);
+          // Handle error from post microservice
+          res.status(500).send('Error creating post');
+        });
+      
+    
+    }
+    else{
+      res.send("Please login first before creating post")
+    }
+    
+  });
+
+
+ 
 
 app.listen(port, () => {
   console.log(`User microservice listening at http://localhost:${port}`);
